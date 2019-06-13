21c7f0a271ea5622027ff3ea06bf81a197b4cf2e
Anantha Amirtharaj Rapheal <Anantha.Rapheal@fadv.com>
2019-06-13 22:26:18 +0530

    Working Branch git

--- sample.java
+++ sample.java
@@ -1,5 +1,7 @@
 class sample {
   public static void main(String[] arguments) {
     System.out.println("Let's do something using Java technology.");
+	System.out.println("Welcome.");
+	System.out.println("Working for Branch Checkout at JenKins.");
   }
 }
